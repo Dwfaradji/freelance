cd9bd9cd6a9def6d3249d0cb903e9590ba22057e
Boucif Faradji <85832124+Dwfaradji@users.noreply.github.com>
2023-06-13 20:49:35 +0200

    fix dataKey

--- next.config.js
+++ next.config.js
@@ -1,8 +1,5 @@
 /** @type {import('next').NextConfig} */
 
-// const nextConfig = {}
-// module.exports = nextConfig
-
 const path = require("path");
 
 module.exports = {
@@ -18,7 +15,6 @@ module.exports = {
     return config;
   },
   env: {
-    KEY_SENDGRID: "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",
-  }
+    KEY_SENDGRID: process.env.KEY_SENDGRID_API,
+  },
 };
-
