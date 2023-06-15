872323426366741325fcb541824980c8fad78f38
Boucif Faradji <85832124+Dwfaradji@users.noreply.github.com>
2023-06-15 12:18:35 +0200

    seo

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,6 +9,28 @@ export const metadata = {
     "Besoin d'un développeur web freelance expérimenté ? Obtenez un site performant, responsive et optimisé pour les moteurs de recherche. Contactez-nous dès maintenant pour un site sur mesure.",
   alternates: {
     canonical: "https://www.devevoke.com/",
+    languages: {
+      fr: "https://www.devevoke.com/",
+    },
+  },
+  robots: {
+    follow: true,
+    index: true,
+  },
+
+  openGraph: {
+    url: "https://www.devevoke.com/",
+    title: "Sites web sur mesure par un développeur web freelance expérimenté",
+    description:
+      "Besoin d'un développeur web freelance expérimenté ? Obtenez un site performant, responsive et optimisé pour les moteurs de recherche. Contactez-nous dès maintenant pour bénéficier d'une solution sur mesure, créée selon vos besoins et attirer davantage de visiteurs.",
+    images: [
+      {
+        url: "https://www.devevoke.com/images/og-image.jpg",
+        width: 1200,
+        height: 630,
+        alt: "Og Image Alt",
+      },
+    ],
   },
 };
 
