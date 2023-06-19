c2e3ec8835045d999c0e4706126880177a5959f3
Boucif Faradji <85832124+Dwfaradji@users.noreply.github.com>
2023-06-19 18:21:56 +0200

    fix bug V.1

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,7 @@
 import React from "react";
 import Home from "@/app/Home/Home";
 import "./Styles/globals.scss";
-import handle from "@/app/api/database";
-import { NextResponse } from "next/server";
 
-const res = NextResponse;
-const req = Request;
-
-export async function dataBase() {
-  return await handle(req, res);
-}
-
-dataBase();
 const Page: React.FC = () => {
   return (
     <main>
