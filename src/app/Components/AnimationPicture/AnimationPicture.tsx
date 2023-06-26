b11222801fad9aa089a0dea48976e74d050d1546
Boucif Faradji <85832124+Dwfaradji@users.noreply.github.com>
2023-06-26 15:48:28 +0200

    add mentions legal+ ajust mobile css

--- src/app/Components/AnimationPicture/AnimationPicture.tsx
+++ src/app/Components/AnimationPicture/AnimationPicture.tsx
@@ -1,18 +1,54 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import "./AnimationPicture.scss";
 import Image from "next/image";
+import { gsap } from "gsap";
 
 interface AnimationPictureProps {
   itemsPicture: any[];
 }
 
-const  AnimationPicture = ({ itemsPicture }: AnimationPictureProps) => {
+const AnimationPicture = ({ itemsPicture }: AnimationPictureProps) => {
+  const sectionRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    const sectionElement = sectionRef.current;
+
+    if (!sectionElement) return;
+
+    const observer = new IntersectionObserver(
+      (entries) => {
+        entries.forEach((entry) => {
+          if (entry.isIntersecting) {
+            const items = sectionElement.querySelectorAll(".item");
+
+            gsap.set(items, { opacity: 0 }); // Masquer tous les éléments au départ
+
+            gsap.fromTo(
+              items,
+              { opacity: 0 },
+              { opacity: 1, duration: 1, stagger: 0.5, ease: "power2.inOut" }
+            ); // Animation de fondu (fade-in) pour chaque élément avec un délai de 0.5 seconde entre eux
+
+            observer.unobserve(entry.target); // Arrêter d'observer une fois que l'animation est déclenchée
+          }
+        });
+      },
+      { threshold: 0.2 } // Définir le seuil d'intersection à 0.2 (lorsque 20% de la section est visible)
+    );
+
+    observer.observe(sectionElement);
+
+    return () => {
+      observer.disconnect(); // Arrêter d'observer lorsque le composant est démonté
+    };
+  }, []);
+
   return (
     <>
       <h2 className="text-h2">Processus de développement web freelance</h2>
-      <div className="about-images">
+      <div className="about-images" ref={sectionRef}>
         {itemsPicture.map((item, i) => (
-          <div key={i} className="item" >
+          <div key={i} className="item">
             <Image
               className="image-container"
               src={item.picture}
