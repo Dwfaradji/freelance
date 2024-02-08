81759f7f1905bd0ee630c8bdfe7d27c911af2eaa
Boucif Faradji <85832124+Dwfaradji@users.noreply.github.com>
2024-02-08 10:56:18 +0100

    fix bug

--- src/context/reducer.js
+++ src/context/reducer.js
@@ -8,33 +8,33 @@ export const initialState = {
 
 export const reducer = (state, action) => {
   switch (action.type) {
-    case "ADD_TEMPLATE":
-      return {
-        ...state,
-        template: action.payload,
-      };
-    case "ADD_COLORS":
-      return {
-        ...state,
-        colors: action.payload,
-      };
-    case "ADD_FONTS":
-      return {
-        ...state,
-        fonts: action.payload,
-      };
-    case "ADD_CHECKBOX":
-      return {
-        ...state,
-        checkbox: action.payload,
-      };
-    case "ADD_FORM":
-      return {
-        ...state,
-        form: action.payload,
-      };
+  case "ADD_TEMPLATE":
+    return {
+      ...state,
+      template: action.payload,
+    };
+  case "ADD_COLORS":
+    return {
+      ...state,
+      colors: action.payload,
+    };
+  case "ADD_FONTS":
+    return {
+      ...state,
+      fonts: action.payload,
+    };
+  case "ADD_CHECKBOX":
+    return {
+      ...state,
+      checkbox: action.payload,
+    };
+  case "ADD_FORM":
+    return {
+      ...state,
+      form: action.payload,
+    };
     // autres actions
-    default:
-      return state;
+  default:
+    return state;
   }
 };
