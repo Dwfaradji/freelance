d3f624cbb046d0e34755d4fe8feb1fe5f6a55b18
Boucif Faradji <85832124+Dwfaradji@users.noreply.github.com>
2024-02-21 15:40:25 +0100

    add template

--- src/app/(pages)/about/page.tsx
+++ src/app/(pages)/about/page.tsx
@@ -1,13 +1,10 @@
 import React from "react";
-import APropos from "@/app/(pages)/about/a_propos";
 import AboutPage from "@/components/sections/AboutPage/AboutPage";
-
 
 const Page = () => {
   return (
     <main>
       <AboutPage />
-
     </main>
   );
 };
