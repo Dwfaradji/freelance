f0fa54653d508448ed6c96ed09436042a40acdd0
Boucif Faradji <85832124+Dwfaradji@users.noreply.github.com>
2024-03-03 17:04:40 +0100

    sitemap

---
+++ src/app/sitemap.ts
@@ -0,0 +1,43 @@
+import { MetadataRoute } from 'next';
+
+export default function sitemap(): MetadataRoute.Sitemap {
+
+  return [
+    {
+      url: 'https://www.devevoke.com/',
+      lastModified: new Date(),
+      changeFrequency: 'monthly',
+      priority: 1,
+    },
+    {
+      url: 'https://www.devevoke.com/a_propos',
+      lastModified: new Date(),
+      changeFrequency: 'monthly',
+      priority: 0.8,
+    },
+    {
+      url: 'https://www.devevoke.com/blog',
+      lastModified: new Date(),
+      changeFrequency: 'weekly',
+      priority: 0.9,
+    },
+    {
+      url: 'https://www.devevoke.com/tarifs',
+      lastModified: new Date(),
+      changeFrequency: 'yearly',
+      priority: 0.6,
+    },
+    {
+      url: 'https://www.devevoke.com/portfolio',
+      lastModified: new Date(),
+      changeFrequency: 'monthly',
+      priority: 0.8,
+    },
+    {
+      url: 'https://www.devevoke.com/contact',
+      lastModified: new Date(),
+      changeFrequency: 'yearly',
+      priority: 0.5,
+    },
+  ];
+}
