c5b973fa1843f99c65094b9ec873ec10b4580651
Boucif Faradji <85832124+Dwfaradji@users.noreply.github.com>
2024-03-06 17:05:27 +0100

    add cookies

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,12 +5,13 @@ import Footer from '@/components/footer';
 import Navbar from '@/components/navbar';
 import Loading from '@/app/loading';
 import GoogleAnalytics from '@/lib/GoogleAnalytics';
-import { SpeedInsights } from "@vercel/speed-insights/next"
+import { SpeedInsights } from '@vercel/speed-insights/next';
 
 import 'swiper/css';
 import 'swiper/css/pagination';
 import 'swiper/css/navigation';
 import 'swiper/css/effect-coverflow';
+import Script from "next/script";
 
 const inter = Inter({ subsets: ['latin'], variable: '--font-sans' });
 
@@ -36,12 +37,17 @@ export default function RootLayout({
 }) {
   return (
     <html lang="fr">
+      <Script
+        type="text/javascript"
+        charSet="UTF-8"
+        src="//cdn.cookie-script.com/s/64964e641363fdb6351948e5bee65388.js"
+      ></Script>
       <GoogleAnalytics GA_TRACKING_ID={String(process.env.GA_TRACKING_ID)} />
       <body className={`${inter.variable} antialiased`}>
         <Navbar />
         <Suspense fallback={<Loading />}>{children}</Suspense>
         <Footer />
-        <SpeedInsights/>
+        <SpeedInsights />
       </body>
     </html>
   );
